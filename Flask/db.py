5cdf13c9aceb94cdfa70ef5a24c0a6d749e86e33
Jennifer_Liu <thejenniferliu@gmail.com>
2023-04-06 21:00:49 -0600

    'begindb'

---
+++ Flask/db.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    post = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
